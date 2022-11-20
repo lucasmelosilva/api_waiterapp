f2a6925c0feba8a94a594561d560351eda9d3835
lucas melo <lucas.melo044@gmail.com>
2022-11-20 18:34:09 -0300

    add conexao com o mongobd

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
 
-const app = express();
+mongoose
+  .connect("mongodb://localhost:27017")
+  .then(() => {
+    console.log("connect success");
+    const app = express();
 
-app.listen(3001, () => {
-  console.log("🚀 server is running on http://localhost:3001");
-});
-
-app.get("/", (req, res) => {
-  res.send("hello");
-});
+    app.listen(3001, () => {
+      console.log("🚀 server is running on http://localhost:3001");
+    });
+  })
+  .catch(() => console.log("err connect"));
