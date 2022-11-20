8521991cc2eb8127b39eeca23463931a41961a65
lucas melo <lucas.melo044@gmail.com>
2022-11-20 16:35:56 -0300

    add express nodemon ts-node

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+import express from "express";
+
+const app = express();
+
+app.listen(3001, () => {
+  console.log("🚀 server is running on http://localhost:3001");
+});
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
