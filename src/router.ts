358902d482d538e13cdaaffe69d95c2e9f255985
lucas melo <lucas.melo044@gmail.com>
2022-11-21 10:39:28 -0300

    add rota de lista categorias

---
+++ src/router.ts
@@ -0,0 +1,47 @@
+import { Router } from "express";
+import { listCategory } from "./app/useCases/categories/listCategory";
+
+export const router = Router();
+
+// List Categories
+router.get("/categories", listCategory);
+
+// Create Category
+router.post("/categories", (req, res) => {
+  res.send("OK");
+});
+
+// List Products
+router.get("/products", (req, res) => {
+  res.send("OK");
+});
+
+// Create Product
+router.post("/products", (req, res) => {
+  res.send("OK");
+});
+
+// Get Products by Category
+router.get("/categories/:categoryId/products", (req, res) => {
+  res.send("OK");
+});
+
+// List Orders
+router.get("/orders", (req, res) => {
+  res.send("OK");
+});
+
+// Create Order
+router.post("/orders", (req, res) => {
+  res.send("OK");
+});
+
+//Change Status Order
+router.patch("/orders/:orderId", (req, res) => {
+  res.send("OK");
+});
+
+// Deleter/Cancel order
+router.delete("/orders/:orderId", (req, res) => {
+  res.send("OK");
+});
