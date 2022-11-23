446856a79539a22f1213e604791e5c4fe81d24e5
lucas melo <lucas.melo044@gmail.com>
2022-11-23 10:17:38 -0300

    add pasta estaticas no express

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-import express from "express";
+import path from "node:path";
+
+import express, { json } from "express";
 import mongoose from "mongoose";
+
+import { router } from "./router";
 
 mongoose
   .connect("mongodb://localhost:27017")
@@ -7,8 +11,16 @@ mongoose
     console.log("connect success");
     const app = express();
 
-    app.listen(3001, () => {
-      console.log("🚀 server is running on http://localhost:3001");
+    app.use(
+      "/uploads",
+      express.static(path.resolve(__dirname, "..", "uploads"))
+    );
+    app.use(json());
+    app.use(router);
+
+    const port = 3001;
+    app.listen(port, () => {
+      console.log(`🚀 server is running on http://localhost:${port}`);
     });
   })
   .catch(() => console.log("err connect"));
