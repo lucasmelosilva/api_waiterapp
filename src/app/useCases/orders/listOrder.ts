b9b949dd1a1fa0942eecb74800b797f7acfc6853
lucas melo <lucas.melo044@gmail.com>
2022-11-24 00:21:09 -0300

    add populate na rota de lista orders

--- src/app/useCases/orders/listOrder.ts
+++ src/app/useCases/orders/listOrder.ts
@@ -3,7 +3,7 @@ import { Order } from "../../models/Order";
 
 export async function listOrder(req: Request, res: Response) {
   try {
-    const order = await Order.find();
+    const order = await Order.find().populate("products.product");
 
     res.json(order);
   } catch (err) {
